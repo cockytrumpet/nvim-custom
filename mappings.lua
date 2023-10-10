a7e7ee8048ee96265831931b3d2c2f929fe9f174
Adam Fowler <cockytrumpet@icloud.com>
2023-10-10 19:31:52 -0400

    fix keymap

--- mappings.lua
+++ mappings.lua
@@ -26,7 +26,7 @@ M.general = {
   },
 
   n = {
-    ["gw"] = { "silent grep! <cword> | cw<CR>", "Grep Word" },
+    ["gw"] = { "<CMD>silent grep! <cword> | cw<CR>", "Grep Word" },
     ["<leader>a"] = {
       function()
         require("neogen").generate()
