33ce707174c4cd872a1a3eb41fd1ca654ee0ac36
Adam Fowler <cockytrumpet@icloud.com>
2023-11-08 11:50:08 -0500

    cleanup

--- chadrc.lua
+++ chadrc.lua
@@ -7,34 +7,28 @@ local highlights = require "custom.highlights"
 M.ui = {
   statusline = {
     theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
-
     -- default/round/block/arrow (separators work only for "default" statusline theme;
     -- round and block will work for the minimal theme only)
     separator_style = "block",
     overriden_modules = nil,
   },
-
   theme = "catppuccin",
   theme_toggle = { "tokyonight", "catppuccin" },
   lsp_semantic_tokens = false,
   hl_override = highlights.override,
   hl_add = highlights.add,
-
   cmp = {
     icons = true,
     lspkind_text = true,
     style = "default", -- default/flat_light/flat_dark/atom/atom_colored
   },
-
   lsp = {
     signature = {
       disabled = true,
       silent = true,
     },
   },
-
   telescope = { style = "bordered" },
-
   extended_integrations = {
     "dap",
     "notify",
@@ -45,23 +39,8 @@ M.ui = {
     "notify",
   },
 }
---[[
-M.settings = {
-  -- cc_size = "130",
-  so_size = 10,
 
-  -- Blacklisted files where cc and so must be disabled
-  blacklist = {
-    "NvimTree",
-    "nvdash",
-    "nvcheatsheet",
-    "terminal",
-    "Trouble",
-    "help",
-  },
-}
-]]
-M.lazy_nvim = require "custom.configs.lazy_nvim" -- config for lazy.nvim startup options
+M.lazy_nvim = require "custom.configs.lazy_nvim"
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
